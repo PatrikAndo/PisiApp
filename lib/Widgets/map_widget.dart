ef09c8bb5abb92af2d7aea49af089f95eb24453d
Botond Horvath <hello@boti7.com>
2019-10-28 18:22:27 +0100

    Upgrade Google Maps plugin 0.2 -> 0.5

--- lib/Widgets/map_widget.dart
+++ lib/Widgets/map_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -18,7 +19,10 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  GoogleMapController mapController;
+  Completer<GoogleMapController> _mapController = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  MarkerId selectedMarker;
+  int _markerIdCounter = 0;
   List<Place> places;
   void Function(Place) onPlaceSelected;
 
@@ -28,33 +32,58 @@ class _MapWidgetState extends State<MapWidget> {
   );
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    _mapController.complete(controller);
 
     DataLoader.loadData().then((List<Place> places) {
       this.places = places;
 
       places.forEach((p) {
-        mapController.addMarker(
-          MarkerOptions(
-            consumeTapEvents: true,
+        if (_markerIdCounter > 10) return;
+
+        final MarkerId markerId = MarkerId('marker_$_markerIdCounter');
+        _markerIdCounter++;
+
+        final Marker marker = Marker(
+            markerId: markerId,
             position: LatLng(p.Lattitude, p.Longitude),
-            icon: BitmapDescriptor.defaultMarker,
-          ),
-        );
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueYellow),
+            onTap: () {
+              setState(() {
+                if (selectedMarker != null) {
+                  markers[selectedMarker] = markers[selectedMarker].copyWith(
+                    iconParam: BitmapDescriptor.defaultMarkerWithHue(
+                        BitmapDescriptor.hueYellow),
+                  );
+                }
+                markers[markerId] = markers[markerId]
+                    .copyWith(iconParam: BitmapDescriptor.defaultMarker);
+              });
+
+              onPlaceSelected(p);
+
+              selectedMarker = markerId;
+            });
+
+        setState(() {
+          markers[markerId] = marker;
+        });
       });
     });
+  }
 
-    mapController.onMarkerTapped.add((Marker m) {
-      int placeId = int.tryParse(m.id.substring(1));
-      if (placeId != null) {
-        Place selectedPlace = places[placeId];
-        onPlaceSelected(selectedPlace);
+  void _onMapTapped(LatLng latLng) {
+    setState(() {
+      if (selectedMarker != null) {
+        markers[selectedMarker] = markers[selectedMarker].copyWith(
+          iconParam:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow),
+        );
       }
-    });
 
-    mapController.onMapTapped.add((LatLng latLng) {
-      onPlaceSelected(null);
+      selectedMarker = null;
     });
+    onPlaceSelected(null);
   }
 
   @override
@@ -69,12 +98,13 @@ class _MapWidgetState extends State<MapWidget> {
     return GoogleMap(
       initialCameraPosition: _defaultPosition,
       onMapCreated: _onMapCreated,
+      onTap: _onMapTapped,
       compassEnabled: false,
       mapToolbarEnabled: false,
       myLocationEnabled: true,
       rotateGesturesEnabled: false,
       tiltGesturesEnabled: false,
-      trackCameraPosition: false,
+      markers: Set<Marker>.of(markers.values),
     );
   }
 }
